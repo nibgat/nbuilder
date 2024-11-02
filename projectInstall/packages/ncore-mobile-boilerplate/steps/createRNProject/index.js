1bc027870f6eca9bf5cbb1a72a75500a3f28194f
lfabl <fab@nibgat.com>
2024-11-03 02:31:58 +0300

    Bugfix: React-Native build error fixed.

--- projectInstall/packages/ncore-mobile-boilerplate/steps/createRNProject/index.js
+++ projectInstall/packages/ncore-mobile-boilerplate/steps/createRNProject/index.js
@@ -3,7 +3,7 @@ const {
 } = require("../../../../utils/tools");
 
 const createRNProject = async (projectName) => {
-    return await execute(`npx react-native init ${projectName}`, {
+    return await execute(`npx @react-native-community/cli init ${projectName}`, {
         successMessage: "✓ React Native project successfully created.",
         startMessage: "React Native project creating...",
         isMultipleParameter: true
